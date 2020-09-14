f56906b0868852bc5b6ce7c62fdfdde60f0b82eb
Fantast-Li <57612431+Fantast-Li@users.noreply.github.com>
2020-09-14 10:21:32 +0800

    Add files via upload

--- TestDemo.java
+++ TestDemo.java
@@ -1,19 +1,54 @@
-public class TestDemo {
-    public static void main(String[]args){
-    Cat lisi=new Cat();
-    lisi.a++;
-    Cat.count++;
-    System.out.println(lisi.a);
-    System.out.println(Cat.count);
-    System.out.println("==========");
-    Cat zhangsan=new Cat();
-    zhangsan.a++;
-    Cat.count++;
-    System.out.println(zhangsan.a);
-    System.out.println(Cat.count);
+package com.li;
+
+class Animal {
+    public String  name ;
+    public Animal (String name) { //有参构造方法
+        this.name = name ;
+    }
+    public void eat() {
+        System.out.println(this.name+"正在吃");
     }
 }
-class Cat{
-     public int a;
-     public static int count;
+class Cat extends Animal {
+    public Cat (String name ){
+        super(name);   //super 向上转型
+    }
+    public void grab (){  //grab 抓
+        System.out.println(this.name+"抓抓板");
+    }
+}
+class Bird extends Animal {
+
+    public Bird(String name) {
+        super(name);
+    }
+    public void fly () {
+        System.out.println(super.name+"会飞");
+    }
+}
+class Dog extends Animal{
+
+    public Dog(String name) {
+        super(name);
+    }
+    public void call () {
+        System.out.println(this.name+"汪汪！");
+    }
+}
+public class TestDemo {
+    public static void main(String[] args) {
+        Cat cat = new Cat("咪咪");
+        cat.eat();
+        cat.grab();
+        Bird bird = new Bird("嘎嘎");
+        bird.fly();
+        bird.eat();
+        bird.name = "haha" ;
+        bird.fly();
+        Dog dog = new Dog("旺财");
+        dog.call();
+        dog.eat();
+
+
+    }
 }
